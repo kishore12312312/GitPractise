908c80a1eb0d14dde66eefa7ec08176b0c430bf9
Kishore <Kishoresm.sm@gmail.com>
2022-12-09 10:10:18 +0530

    v 1

---
+++ GitPractise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitPractise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
